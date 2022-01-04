6333667a334385ca3b93008a87b695b1cbced4ad
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-01-05 00:09:54 +0200

    Changes the color of the car when picking up a package.

--- Assets/Scenes/Collision.cs
+++ Assets/Scenes/Collision.cs
@@ -7,17 +7,32 @@ public class Collision : MonoBehaviour
 
     float destroyDelay = 0.5f;
 
+
+    [SerializeField]
+    Color32 packagePickedUpColor = new Color32(1, 1, 1, 0);
+
+    [SerializeField]
+    Color32 noPackageColor = new Color32(0, 0, 0, 0);
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collition detected.");
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Package")
         {
             Debug.Log("Package picked up !");
+
+            this.spriteRenderer.color = this.packagePickedUpColor;
 
             Destroy(other.gameObject, destroyDelay);
         }
