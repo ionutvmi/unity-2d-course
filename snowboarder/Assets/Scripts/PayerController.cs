e44aaa916b8540cbac6efd407d453c980d632261
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-01-11 00:08:32 +0200

    Adds the torque to the player controller.

--- snowboarder/Assets/Scripts/PayerController.cs
+++ snowboarder/Assets/Scripts/PayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PayerController : MonoBehaviour
 {
+    [SerializeField]
+    float torqueAmount = 1f;
+
+    Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.rb2d.AddTorque(torqueAmount);
+        } 
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.rb2d.AddTorque(-torqueAmount);
+        }
     }
 }
