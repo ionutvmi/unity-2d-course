19e165e67f931089f9c3bdc91b09045207820c61
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-01-13 00:10:34 +0200

    Creates the particle effects for the finish the crash effect.

--- snowboarder/Assets/Scripts/CrashDetector.cs
+++ snowboarder/Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField]
+    float reloadDelay = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
