a5833ab8877d5f8191f7044c2d790fda78998d19
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-01-13 00:46:31 +0200

    Adds the triggering logic for the particle effects.

--- snowboarder/Assets/Scripts/CrashDetector.cs
+++ snowboarder/Assets/Scripts/CrashDetector.cs
@@ -8,10 +8,14 @@ public class CrashDetector : MonoBehaviour
     [SerializeField]
     float reloadDelay = 0.5f;
 
+    [SerializeField]
+    ParticleSystem carshEffect;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
+            carshEffect.Play();
             Invoke("ReloadScene", reloadDelay);
         }
     }
--- snowboarder/Assets/Scripts/FinishLine.cs
+++ snowboarder/Assets/Scripts/FinishLine.cs
@@ -8,10 +8,14 @@ public class FinishLine : MonoBehaviour
     [SerializeField]
     float reloadDelay = 1f;
 
+    [SerializeField]
+    ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", reloadDelay);
         }
     }
