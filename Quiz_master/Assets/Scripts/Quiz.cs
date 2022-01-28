32a05abd39e232c40871f636de64f437d27c7482
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-01-29 00:11:20 +0200

    Makes the answer buttons dynamic.

--- Quiz_master/Assets/Scripts/Quiz.cs
+++ Quiz_master/Assets/Scripts/Quiz.cs
@@ -12,11 +12,19 @@ public class Quiz : MonoBehaviour
     [SerializeField]
     QuestionSO question;
 
+    [SerializeField]
+    GameObject[] answerButtons;
 
 
     void Start()
     {
         this.questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
     }
 
 }
