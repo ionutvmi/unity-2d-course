434d22b64a45a268a9599c57c3a43e0bfefe3254
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-02-01 23:58:36 +0200

    Adds the logic for enabling and disabling the button states.

--- Quiz_master/Assets/Scripts/Quiz.cs
+++ Quiz_master/Assets/Scripts/Quiz.cs
@@ -26,6 +26,18 @@ public class Quiz : MonoBehaviour
 
     void Start()
     {
+        DisplayQuestion();
+    }
+
+    void GetNextQuestion()
+    {
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    void DisplayQuestion()
+    {
         this.questionText.text = question.GetQuestion();
 
         for (int i = 0; i < answerButtons.Length; i++)
@@ -33,20 +45,44 @@ public class Quiz : MonoBehaviour
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
         }
+        SetButtonState(true);
     }
 
-    public void OnAnswerSelected(int index) {
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button btn = answerButtons[i].GetComponent<Button>();
+            btn.interactable = state;
+        }
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
         correctAnswerIndex = question.GetCorrectAnswerIndex();
 
-        if (index == correctAnswerIndex) {
+        if (index == correctAnswerIndex)
+        {
             questionText.text = "Correct!";
             Image buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
-        } else {
+        }
+        else
+        {
             questionText.text = "Incorrect !";
             Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
+        SetButtonState(false);
     }
 
 }
